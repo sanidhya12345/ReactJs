9f150a1a71bcb9727f64ac5b4db48b9c076b575c
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-09-18 17:53:36 +0530

    react

---
+++ basic_app/src/Subscribe.js
@@ -0,0 +1,24 @@
+import "./Subscribe.css";
+const Subscribe=()=>(
+    <div className="container">
+        <h1>
+            SUBSCRIBE
+        </h1>
+        <h3>
+            Sign up with your email address to recieve news and updates.
+        </h3>
+        <div className="input_btn">
+            <div className="input">
+                <input text="" placeholder="First Name"></input>
+                <input text="" placeholder="Last Name"></input>
+                <input text="" placeholder="E-mail"></input>
+            </div>
+
+            <div className="button">
+                
+            </div>
+        </div>
+    </div>
+);
+
+export default Subscribe;
--- basic_app/src/index.js
+++ basic_app/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Subscribe from './Subscribe';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App></App>
+    <Subscribe></Subscribe>
   </React.StrictMode>
 );
 
