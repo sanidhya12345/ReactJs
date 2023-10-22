18a7f7a2e0e9cccf8fbbcd60d7637f31ec9c2c9b
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-10-22 17:30:10 +0530

    react

--- simple_portfolio/src/index.js
+++ simple_portfolio/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import NavBar from './NavBar';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-   <NavBar></NavBar>
-  </React.StrictMode>
-);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./NavBar";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BrowserRouter>
+<NavBar></NavBar>
+</BrowserRouter>);
