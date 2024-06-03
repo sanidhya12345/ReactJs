5174b0550e0be9c66f31ef16f829c98acdb8309c
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-06-03 10:56:22 +0530

    map

---
+++ JavaScript_Refresher/FunctionalProgramming/map.js
@@ -0,0 +1,9 @@
+let arr=[1,2,3,4,5]
+const array=arr.map(number=>number*number)
+console.log(array)
+
+
+let countries=["India","Japan","China","England"];
+const country=countries.map(value=>value.charAt(0))
+console.log(country)
+
