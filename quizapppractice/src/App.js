ce368df19d8099650095426210416fe203c15720
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-06-05 17:40:49 +0530

    qa practice

---
+++ quizapppractice/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <div className="quiz_container">
+          <h2>Q1.What will be the sum of 2+2?</h2>
+          <input type="checkbox"></input>
+          <input type="checkbox"></input>
+          <input type="checkbox"></input>
+          <input type="checkbox"></input>
+       </div>
+    </div>
+  );
+}
+
+export default App;
