c78066f4ec6d9cd927170fc15de5e0cda96e9c28
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-06-06 11:23:06 +0530

    update

--- quizapppractice/src/App.js
+++ quizapppractice/src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  const [value,setValue]=useState("");
   return (
     <div className="App">
        <div className="quiz_container">
           <h2>Q1.What will be the sum of 2+2?</h2>
-          <input type="checkbox"></input>
-          <input type="checkbox"></input>
-          <input type="checkbox"></input>
-          <input type="checkbox"></input>
+          <div>
+          <div>
+            <label>A.8</label>
+             <input type="radio" id="option1" value="option1" onChange={()=>{
+              setValue("option1")
+             }}checked={value==="option1"} ></input>
+          </div>
+          <div>
+          <label>B.4</label>
+             <input type="radio" id="option2" value="option2" onChange={()=>{
+              setValue("option2")
+             }} checked={value==="option2"}></input>
+          </div>
+          <div>
+          <label>C.3</label>
+             <input type="radio" id="option3" value="option3" onChange={()=>{
+              setValue("option3")
+             }} checked={value==="option3"}></input>
+          </div>
+          <div>
+          <label>D.5</label>
+             <input type="radio" id="option4" value="option4" onChange={()=>{
+              setValue("option4")
+             }} checked={value==="option4"}></input>
+          </div>
+          </div>
        </div>
     </div>
   );
